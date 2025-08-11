9957d399c24b21d579d378ecf44abab170ec5937
Guru1316 <guruprasadgdr1@gmail.com>
2025-08-12 00:08:30 +0530

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Astune-Vector/Vector-Dev-Tools/VectorConvert/', // IMPORTANT: exact repo + folder path
+  base: '/VectorConvert/',  // Important for GitHub Pages
   plugins: [react()],
 })
